37d2dcfe2aaa9309acc0c0ec0f08e724e74a39c8
Kristina Taing <kataing12@gmail.com>
2019-06-26 12:17:33 -0700

    [feat] built server app withouth CRUD functionalities

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const parse = require('body-parser');
+const path = require('path');
 
+const app = express();
+const port = 3333;
+
+app.use(parse.json());
+app.use(parse.urlencoded( { extended: true }));
+app.use(express.static(path.join(__dirname, './client/dist/')));
+
+// app.get()
+
+app.listen((port) => console.log(`Checkout App listening on port ${port}.`));
 
