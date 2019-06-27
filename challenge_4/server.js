a3fc9b358b51544b284947e86e4e2cc28d91cc30
Kristina Taing <kataing12@gmail.com>
2019-06-27 11:37:48 -0700

    [feat] built functioning server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const parse = require('body-parser');
+const path = require('path');
 
+const app = express();
+const port = 8000;
 
+app.use(parse.json());
+app.use(parse.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
