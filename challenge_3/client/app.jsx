b8a25a397edd31d80a184e25e51913ec546b9b47
Kristina Taing <kataing12@gmail.com>
2019-06-26 18:03:35 -0700

    [prog] configured front end to alter data in database - base requirements completed

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,29 +10,65 @@ class App extends React.Component {
       f2: false,
       f3: false,
       confirmation: false,
-      name: '',
-      email: '',
-      password: '',
-      address1: '',
-      address2: '',
-      city: '',
-      state: '',
-      zip: '',
-      phone: '',
-      creditcard: '',
-      expirydate: '',
-      cvv: '',
-      billingzip: '',
-      purchase: ''
+      id: null,
+      name: null,
+      email: null,
+      password: null,
+      address1: null,
+      address2: null,
+      city: null,
+      state: null,
+      zip: null,
+      phone: null,
+      creditcard: null,
+      expirydate: null,
+      cvv: null,
+      billingzip: null,
+      purchase: null
     }
     this.handleCheckoutClick = this.handleCheckoutClick.bind(this);
     this.handleNext1Click = this.handleNext1Click.bind(this);
     this.handleNext2Click = this.handleNext2Click.bind(this);
     this.handleNext3Click = this.handleNext3Click.bind(this);
+    this.handlePurchaseClick = this.handlePurchaseClick.bind(this);
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.post = this.post.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  post() {
+    const {
+      name, email, password, address1, address2, city, state, zipcode,
+      phone, creditcard, expirydate, cvv, billingzip, purchase
+    } = this.setState;
+    axios
+      .post('route/api', {
+        name, email, password, address1, address2, city, state, zipcode,
+        phone, creditcard, expirydate, cvv, billingzip, purchase
+      })
+      .then(({ data }) => {
+        console.log('your checkout has been initiated.');
+        this.setState({ id: data });
+      })
+      .catch(() => console.log('your checkout could not be initiated.'))
+  }
+
+  update() {
+    const {
+      name, email, password, address1, address2, city, state, zipcode,
+      phone, creditcard, expirydate, cvv, billingzip, purchase
+    } = this.state;
+    axios
+      .put(`route/api/${this.state.id}`, {
+        name, email, password, address1, address2, city, state, zipcode,
+        phone, creditcard, expirydate, cvv, billingzip, purchase
+      })
+      .then(() => console.log('your checkout progress has been saved.'))
+      .catch(() => console.log('your checkout progress has not been saved.'))
   }
 
   handleCheckoutClick() {
+    this.post();
     this.setState({
       home: !this.state.home,
       f1: !this.state.f1
@@ -43,6 +80,7 @@ class App extends React.Component {
       f1: !this.state.f1,
       f2: !this.state.f2
     })
+    this.update();
   }
 
   handleNext2Click() {
@@ -50,12 +88,21 @@ class App extends React.Component {
       f2: !this.state.f2,
       f3: !this.state.f3
     })
+    this.update();
   }
 
   handleNext3Click() {
     this.setState({
       f3: !this.state.f3,
       confirmation: !this.state.confirmation
+    })
+    this.update();
+  }
+
+  handlePurchaseClick() {
+    this.setState({
+      confirmation: !this.state.confirmation,
+      home: !this.state.home
     })
   }
 
@@ -68,10 +115,12 @@ class App extends React.Component {
   home() {
     if (this.state.home) {
       return (
-        <div>
-          <h3>This is home</h3>
+        <span>
+          <div>
+            <h3>This is home</h3>
+          </div>
           <button onClick={this.handleCheckoutClick}>Begin Checkout</button>
-        </div>
+        </span>
       )
     }
   }
@@ -79,22 +128,24 @@ class App extends React.Component {
   f1() {
     if (this.state.f1) {
       return (
-        <form id='f1' >
-          <h3>TGeneral Information</h3>
-          <div>
-            <div>Name: </div>
-            <input id='name' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>Email: </div>
-            <input id='email' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>Password: </div>
-            <input id='password' type='password' onChange={this.handleOnChange}></input>
-          </div>
+        <span>
+          <form id='f1' >
+            <h3>General Information</h3>
+            <div>
+              <div>Name: </div>
+              <input id='name' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>Email: </div>
+              <input id='email' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>Password: </div>
+              <input id='password' type='password' onChange={this.handleOnChange}></input>
+            </div>
+          </form>
           <button id='nextbutton1' onClick={this.handleNext1Click}>Next</button>
-        </form>
+        </span>
       )
     }
   }
@@ -102,34 +153,36 @@ class App extends React.Component {
   f2() {
     if (this.state.f2) {
       return (
-        <form id='f2'>
-          <h3>Shipping Information</h3>
-          <div>
-            <div>Address Line 1: </div>
-            <input id='address1' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>Address Line 2: </div>
-            <input id='address2' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>City</div>
-            <input id='city' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>State</div>
-            <input id='state' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>Zipcode</div>
-            <input id='zipcode' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>Phone#</div>
-            <input id='phone' type='text'></input>
-          </div>
+        <span>
+          <form id='f2'>
+            <h3>Shipping Information</h3>
+            <div>
+              <div>Address Line 1: </div>
+              <input id='address1' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>Address Line 2: </div>
+              <input id='address2' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>City</div>
+              <input id='city' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>State</div>
+              <input id='state' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>Zipcode</div>
+              <input id='zipcode' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>Phone#</div>
+              <input id='phone' type='text'></input>
+            </div>
+          </form>
           <button id='nextbutton2' onClick={this.handleNext2Click}>Next</button>
-        </form>
+        </span>
       )
     }
   }
@@ -137,26 +190,28 @@ class App extends React.Component {
   f3() {
     if (this.state.f3) {
       return (
-        <form id='f3'>
-          <h3>Billing Information</h3>
-          <div>
-            <div>Creditcard#: </div>
-            <input id='creditcard' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>Exp: </div>
-            <input id='expirydate' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>CVV</div>
-            <input id='cvv' type='text' onChange={this.handleOnChange}></input>
-          </div>
-          <div>
-            <div>Billing Zipcode</div>
-            <input id='billingzip' type='text' onChange={this.handleOnChange}></input>
-          </div>
+        <span>
+          <form id='f3'>
+            <h3>Billing Information</h3>
+            <div>
+              <div>Creditcard#: </div>
+              <input id='creditcard' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>Exp: </div>
+              <input id='expirydate' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>CVV</div>
+              <input id='cvv' type='text' onChange={this.handleOnChange}></input>
+            </div>
+            <div>
+              <div>Billing Zipcode</div>
+              <input id='billingzip' type='text' onChange={this.handleOnChange}></input>
+            </div>
+          </form>
           <button id='nextbutton3' onClick={this.handleNext3Click}>Next</button>
-        </form>
+        </span>
       )
     }
   }
@@ -172,7 +227,7 @@ class App extends React.Component {
   confirmation() {
     if (this.state.confirmation) {
       return (
-        <div>
+        <span>
           <h3>This is confirmation</h3>
           <h3>General Information</h3>
           <div>
@@ -229,8 +284,8 @@ class App extends React.Component {
             <div>Billing Zipcode</div>
             <span>{this.state.billingzip}</span>
           </div>
-          <button>Purchase</button>
-        </div>
+          <button id='purchasebutton' onClick={this.handlePurchaseClick}>Purchase</button>
+        </span>
       )
     }
   }
