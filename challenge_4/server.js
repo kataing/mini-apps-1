56fdba974ff2ec9dde343ea556ef45ce4a3551f9
Kristina Taing <kataing12@gmail.com>
2019-06-27 20:57:35 -0700

    [feat] building in end game logic

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -9,9 +9,45 @@ app.use(parse.json());
 app.use(parse.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
-// sends client a new board
+let board;
+let colTracker = {};
+
+checkEndGame = (r, c) => {
+  if(checkHorizontal(r)) {
+    return true;
+  }
+  return false;
+}
+
+checkHorizontal = (r) => {
+  let count = 0;
+  for(let i = 0; i < r.length; i++) {
+    if(this.state.board[r][i]) {
+      count ++;
+    } else {
+      count = 0;
+    }
+    if(count === 4) {
+      return true;
+    }
+  }
+  return false;
+}
+
+updateBoard = (c, p) => {
+  if(colTracker[c]) {
+    colTracker[c] ++;
+  } else {
+    colTracker[c] = 1;
+  }
+  const r = 6 - colTracker[c];
+  board[r][c] = Number(p);
+
+  return r;
+}
+
 app.get('/api', (req, res) => {
-  const board = [
+  board = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -22,9 +58,13 @@ app.get('/api', (req, res) => {
   res.status(200).send(board);
 })
 
-app.post('/api/:id', (req, res) => {
-  console.log(req.params);
-  req.status(201).status('posted!');
+app.post('/api/:column', (req, res) => {
+  const { column } = req.params;
+  const { player } = req.query;
+  const row = updateBoard(column, player);
+  const endGame = checkEndGame(row, column);
+
+  res.status(201).send({ board, endGame });
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
